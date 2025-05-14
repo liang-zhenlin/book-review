3a98032c6bd122c577a6f25e0a446d1bcadad47c
liang-zhenlin <liangzhenlin0409@163.com>
2025-05-14 17:16:19 +0800

    add cache

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class BookController extends Controller
 {
@@ -12,10 +13,26 @@ class BookController extends Controller
      */
     public function index(Request $request)
     {
-        $title = $request->input("title");
+        $title = $request->input('title');
+        $filter = $request->input('filter', '');
+
         $books = Book::when(
             $title,
-            fn($query, $title) => $query->title($title))->get();
+            fn($query, $title) => $query->title($title));
+
+        $books = match($filter) {
+            'popular_last_month' => $books->popularLastMonth(),
+            'popular_last_6month' => $books->popularLast6Months(),
+            'highest_rated_last_month' => $books->highestRatedLastMonth(),
+            'highest_rated_last_6month' => $books->highestRatedLast6Months(),
+            default => $books->latest()
+        };
+
+        // $books = $books->get();
+        $cacheKey = 'books:' . $filter . ':' . $title;
+
+        $books = cache()->remember($cacheKey, 3600, fn() => $books->get());
+
         return view("books.index", ['books' => $books]);
     }
 
@@ -38,9 +55,16 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Book $book)
     {
-        //
+        return view(
+            '/books.show',
+            [
+                'book' => $book->load([
+                    'reviews' => fn($query) => $query->latest()
+                ])
+            ]
+        );
     }
 
     /**
